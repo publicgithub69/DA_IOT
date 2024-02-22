285503aace824cd4da918b128bf8e1984ac90bd8
publicgithub69 <160835075+publicgithub69@users.noreply.github.com>
2024-02-22 14:17:57 +0530

    Create C#_oop.cs

---
+++ C#_oop.cs
@@ -0,0 +1,42 @@
+using System;
+class programme {
+    
+
+    static void Main(string[] args) {
+        
+        //Format 1
+        Console.WriteLine("The number of argumen passed is {0}",args.Length);
+        //Format 2
+        Console.WriteLine($"The number of argumen passed is {args.Length}");
+        
+        vehicle vehicle1 = new vehicle();
+    
+  }
+  
+  
+}
+
+class Object
+{
+    public int  var1 = 10;
+    private int  var2 = 50;
+    
+    public int Weight = 00;
+    
+
+    
+}
+class vehicle :Object
+{
+    
+    
+}
+
+class audi : vehicle
+{
+    
+    
+}
+
+
+
