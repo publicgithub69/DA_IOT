6a5c1c0cfb4b76e87c0271f20355c2827e229eda
publicgithub69 <160835075+publicgithub69@users.noreply.github.com>
2024-02-22 13:47:56 +0530

    Create lcs.c

---
+++ lcs.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+
+// i and j is the startign indices of these two array
+// i ,j is initially 0
+int max(int a,int b)
+{
+    a>b ? a : b;
+}
+
+char A [3] = "bd0"; 
+char B [5] = "abcd0";
+    
+int lcs(int i,int j)
+{
+    if(A[i]== 0 || B[j]==0)
+    {
+        return 0;
+    }
+    else if(A[i]==B[j])
+    {
+        return 1+lcs(i+1,j+1);
+    }
+    else
+    {
+        return max(lcs(i+1,j),lcs(i,j+1));              
+    }
+    
+    
+}
+
+int main()
+{
+
+    
+    printf("hellow world! \n");
+    printf("The max matching case: %d \n",lcs(0,0));
+    
+    return 0;
+}
